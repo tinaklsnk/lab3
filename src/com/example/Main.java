f7fb5ef5e8a0e32983008cba6fb1e60d628ad838
tinaklsnk <valentaine29012002@gmail.com>
2021-03-10 23:52:48 +0200

    added Cities.txt

--- src/com/example/Main.java
+++ src/com/example/Main.java
@@ -1,10 +1,10 @@
-
 package com.example;
 
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 
 public class Main {
+    
 
     @SerializedName("temp")
     @Expose
