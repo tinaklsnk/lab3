79eb1bf1f0f830a0153918eb92e138f75bdf6440
tinaklsnk <valentaine29012002@gmail.com>
2021-03-12 14:59:51 +0200

    fixed Program.main()

--- src/JSON.java
+++ src/JSON.java
@@ -19,13 +19,14 @@ public class JSON {
         System.out.println("GET DONE");
     }
 
-    private static void sendGET() throws IOException {
+    public static String sendGET() throws IOException {
         URL obj = new URL(GET_URL);
         HttpURLConnection con = (HttpURLConnection) obj.openConnection();
         con.setRequestMethod("GET");
         con.setRequestProperty("User-Agent", USER_AGENT);
         int responseCode = con.getResponseCode();
         System.out.println("GET Response Code :: " + responseCode);
+        String r="";
         if (responseCode == HttpURLConnection.HTTP_OK) {
             BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
             String inputLine;
@@ -34,15 +35,23 @@ public class JSON {
                 response.append(inputLine);
             }
             in.close();
-            System.out.println(response.toString());
             Gson gson = (new GsonBuilder()).excludeFieldsWithoutExposeAnnotation().create();
             WeatherData wdata = (WeatherData) gson.fromJson(response.toString(), WeatherData.class);
+            for (int i = 0; i < 40; i++) {
+               // System.out.println(wdata.getList().get(i).getDtTxt());
+
+            }
+            //System.out.println(response.toString());
+            r = response.toString();
+            //System.out.println(r);
         } else {
             System.out.println("GET request not worked");
         }
+        return r;
     }
 
-
-
-
+    public static WeatherData fromJson(String jsonText){
+        WeatherData response= new Gson().fromJson(jsonText,WeatherData.class);
+        return response;
+    }
 }
--- src/Program.java
+++ src/Program.java
@@ -1,5 +1,35 @@
-public class Program {
-    public static void main(String[] args) {
+import com.example.WeatherData;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Program {
+    public static final int k = 40;
+    public static void main(String[] args) throws IOException {
+        String text = JSON.sendGET();
+        System.out.println("------------------------");
+        Gson gson = (new GsonBuilder()).excludeFieldsWithoutExposeAnnotation().create();
+        WeatherData wdata = (WeatherData) gson.fromJson(text, WeatherData.class);
+        for (int i = 0; i < 40; i++) {
+            System.out.println(wdata.getList().get(i).getDtTxt());
+        }
+
+    }
+
+    public static void city()
+    {
+        String city;
+        FileReader read = null;
+        try {
+            read = new FileReader("cities.txt");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        Scanner scanner = new Scanner(read);
+        city = scanner.nextLine();
     }
 }
